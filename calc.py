ec96151ad497eca2b422d507fbb0e3934eb8efb5
Amr <akkk33@protonmail.com>
2018-06-28 07:02:27 +0200

    Engaged in

--- calc.py
+++ calc.py
@@ -1,18 +1,22 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5.Qt import QApplication, QDialog, QLCDNumber, QMainWindow
 from PyQt5.uic import loadUi
 
 
-class PyCalc(QMainWindow):
+class PyCalc(QMainWindow, QLCDNumber):
+    operation = 0
+    Output = QLCDNumber
+
     def __init__(self):
         super(PyCalc, self).__init__()
         loadUi("Assets/design.ui", self)
         self.setWindowTitle("PyCalc")
+        self.zero.clicked.connect(self.zero_btn)
 
     @pyqtSlot()
-    def on_push_button(self):
-        pass
+    def zero_btn(self):
+        QLCDNumber.intValue(self)
 
 
 app = QApplication(sys.argv)
