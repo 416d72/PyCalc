7455666981b25e3f723e9d8fb82a855b248749f6
akkk33 <akkk33@protonmail.com>
2018-06-29 03:19:17 +0200

    Done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'PyCalc',
+    version = '1.0',
+    packages = ['calc.py'],
+    url = 'https://github.com/@416d72/PyCalc',
+    license = 'MIT',
+    author = 'amr',
+    author_email = '@416d72@pm.me',
+    description = ''
+)
